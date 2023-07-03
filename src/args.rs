f232cde32ec6a426fe30a51747aa8aab84450d6e
Sócrates Díaz <sdsxd1800@gmail.com>
2023-07-02 22:33:28 -0400

    feat: add clap dependency and arguments

--- src/args.rs
+++ src/args.rs
@@ -1,1 +1,26 @@
+use clap::Parser;
 
+#[derive(Args)]
+struct EncodeArgs {
+    file_path: String,
+    chunk_type: String,
+    message: String,
+    output: Option<String>,
+}
+
+#[derive(Args)]
+struct DecodeArgs {
+    file_path: String,
+    chunk_type: String,
+}
+
+#[derive(Args)]
+struct RemoveArgs {
+    file_path: String,
+    chunk_type: String,
+}
+
+#[derive(Args)]
+struct PrintArgs {
+    file_path: String,
+}
